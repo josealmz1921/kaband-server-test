dfdded162a2741dacf2c387bfa1aecef3a144fd5
josealmz <joseharry1921@gmail.com>
2022-03-31 23:00:13 -0600

    actualizacion en funciones para modificar stock

--- functions/modificarStock.js
+++ functions/modificarStock.js
@@ -2,14 +2,34 @@ const Productos = require('../models/Productos');
 const Clientes = require('../models/Clientes');
 const Reportes = require('../models/Reportes');
 const Movimientos = require('../models/Movimientos');
+const groupBy = require('lodash/groupBy');
 
+const saveNewStock = async (stock, id) => {
+    try {
+
+        await Productos.findByIdAndUpdate({ _id: id }, {
+            $set: {
+                almacenes: stock
+            }
+        }, { new: true })
+
+        return true;
+
+    } catch (error) {
+
+        console.log(error);
+
+        return false;
+
+    }
+}
 
 exports.validarStock = async (productos) => {
 
     const validacion = await Promise.all(
-        await productos.map( async producto => {
+        await productos.map(async producto => {
 
-            const productoFind = await Productos.findById({_id:producto._id});
+            const productoFind = await Productos.findById({ _id: producto._id });
             const almacenDesc = productoFind.almacenes.filter(almacen => almacen.id === producto.almacen);
             const nuevaCantidadAlmacen = Number(almacenDesc[0].cantidad) - producto.cantidadVenta;
 
@@ -22,55 +42,78 @@ exports.validarStock = async (productos) => {
 
 }
 
-exports.descontarStock = async (productos,usuario) => {
+exports.descontarStock = async (productos, usuario) => {
 
+    const productosAgrupados = groupBy(productos, '_id');
     await Promise.all(
+        await Object.values(productosAgrupados).map(async productos => {
 
-        await productos.map( async producto => {
+            const productoFind = await Productos.findById({ _id: productos[0]._id });
 
-            const productoFind = await Productos.findById({_id:producto._id});
-            const almacenDesc = productoFind.almacenes.filter(almacen => almacen.id === producto.almacen);
-            const nuevaCantidadAlmacen = Number(almacenDesc[0].cantidad) - producto.cantidadVenta;
-            const almacenesProducto = productoFind.almacenes;
+            const resultado = await Promise.all(
+                productos.map(async producto => {
 
-            let nuevoMovimiento = {};
-            let almacenesList = almacenesProducto.map(almacen => {
-                let datos = {};
-                if(almacen.id == producto.almacen){
-                    datos.almacen = almacen.id;
-                    datos.despues = parseInt(nuevaCantidadAlmacen);
-                    datos.antes = parseInt(almacen.cantidad);
-                    datos.tipo = 'vendido';
-                }else{
-                    datos.almacen = almacen.id;
-                    datos.despues = parseInt(almacen.cantidad);
-                    datos.antes = parseInt(almacen.cantidad);
-                    datos.tipo = 'vendido';
+                    const almacenDesc = productoFind.almacenes.filter(almacen => almacen.id === producto.almacen);
+                    const nuevaCantidadAlmacen = Number(almacenDesc[0].cantidad) - producto.cantidadVenta;
+                    const almacenesProducto = productoFind.almacenes;
+
+                    let nuevoMovimiento = {};
+                    let almacenesList = almacenesProducto.map(almacen => {
+                        let datos = {};
+                        if (almacen.id == producto.almacen) {
+                            datos.almacen = almacen.id;
+                            datos.despues = parseInt(nuevaCantidadAlmacen);
+                            datos.antes = parseInt(almacen.cantidad);
+                            datos.tipo = 'vendido';
+                        } else {
+                            datos.almacen = almacen.id;
+                            datos.despues = parseInt(almacen.cantidad);
+                            datos.antes = parseInt(almacen.cantidad);
+                            datos.tipo = 'vendido';
+                        }
+
+                        return datos;
+                    })
+
+
+                    nuevoMovimiento.almacenes = almacenesList;
+                    nuevoMovimiento.producto = productoFind.nombre;
+                    nuevoMovimiento.productoId = producto._id;
+                    nuevoMovimiento.usuario = usuario;
+                    const movimiento = new Movimientos(nuevoMovimiento);
+                    await movimiento.save();
+
+                    let nuevosAlmacenes = productoFind.almacenes.map(almacen => {
+                        if (almacen.id === producto.almacen) {
+                            almacen.cantidad = nuevaCantidadAlmacen;
+                            return almacen;
+                        }
+                        return null;
+                    });
+
+                    nuevosAlmacenes = nuevosAlmacenes.filter(item => item !== null);
+
+                    return nuevosAlmacenes[0]
+
+                })
+            )
+
+            const newStock = productoFind.almacenes.map(almacen => {
+
+                const findAlmacen = resultado.find(item => item.id === almacen.id);
+
+                if (findAlmacen) {
+                    return findAlmacen
                 }
-                    
-                return datos;
-            })
-            
 
-            nuevoMovimiento.almacenes = almacenesList;
-            nuevoMovimiento.producto = productoFind.nombre;
-            nuevoMovimiento.productoId = producto._id;
-            nuevoMovimiento.usuario = usuario;
-            const movimiento = new Movimientos(nuevoMovimiento);
-            await movimiento.save();
-
-            const nuevosAlmacenes = productoFind.almacenes.map( almacen => {
-                if(almacen.id === producto.almacen){
-                    almacen.cantidad = nuevaCantidadAlmacen;
-                }
                 return almacen;
-            });
-
-            await Productos.findByIdAndUpdate({_id:producto._id},{
-                $set:{
-                    almacenes:nuevosAlmacenes
-                }
             })
+
+            await Promise.all(
+                await resultado.map(async item => {
+                    await saveNewStock(newStock, productos[0]._id);
+                })
+            )
 
         })
     )
@@ -81,36 +124,36 @@ exports.descontarStockOnline = async (productos) => {
 
     await Promise.all(
 
-        await productos.map( async producto => {
-            
+        await productos.map(async producto => {
+
 
             let cantidad = producto.cantidad;
             const productoColor = producto.vinculados.find(item => item.color.toString() === producto.color.toString());
 
-            const productoFind = await Productos.findById({_id:productoColor.id});
+            const productoFind = await Productos.findById({ _id: productoColor.id });
 
-            const ordenandoArray = productoFind.almacenes.sort(function (a, b){
-                return  Number(b.cantidad) - Number(a.cantidad) ;
+            const ordenandoArray = productoFind.almacenes.sort(function (a, b) {
+                return Number(b.cantidad) - Number(a.cantidad);
             });
-            
+
             const nuevosAlmacenes = ordenandoArray.map(almacen => {
 
                 let stockNuevo = almacen.cantidad
-                
-                if(almacen.cantidad >= cantidad){
+
+                if (almacen.cantidad >= cantidad) {
                     stockNuevo = almacen.cantidad - cantidad;
                     cantidad = 0;
-                }else{
-                    if(almacen.cantidad < cantidad){
-                        stockNuevo = cantidad - almacen.cantidad ;
-                        cantidad = cantidad - almacen.cantidad ;
+                } else {
+                    if (almacen.cantidad < cantidad) {
+                        stockNuevo = cantidad - almacen.cantidad;
+                        cantidad = cantidad - almacen.cantidad;
                     }
                 }
 
                 almacen.cantidad = Number(stockNuevo);
-                
+
                 return almacen
-                
+
             })
 
             let ordenandoArrayMenor = [];
@@ -121,10 +164,10 @@ exports.descontarStockOnline = async (productos) => {
             ordenandoArrayMenor.push(validacion2);
             const validacion3 = nuevosAlmacenes.find(item => item.nombre === 'Bodega');
             ordenandoArrayMenor.push(validacion3);
-            
-            await Productos.findByIdAndUpdate({_id:productoColor.id},{
-                $set:{
-                    almacenes:ordenandoArrayMenor
+
+            await Productos.findByIdAndUpdate({ _id: productoColor.id }, {
+                $set: {
+                    almacenes: ordenandoArrayMenor
                 }
             })
 
@@ -133,72 +176,96 @@ exports.descontarStockOnline = async (productos) => {
 
 }
 
-exports.devolverStock = async (productos,usuario) => {
+exports.devolverStock = async (productos, usuario) => {
 
+    const productosAgrupados = groupBy(productos, '_id');
     await Promise.all(
-        await productos.map( async producto => {
+        await Object.values(productosAgrupados).map(async productos => {
 
-            const productoFind = await Productos.findById({_id:producto._id});
-            const almacenDesc = productoFind.almacenes.filter(almacen => almacen.id === producto.almacen);
-            const nuevaCantidadAlmacen = Number(almacenDesc[0].cantidad) + Number(producto.cantidadVenta);
-            const almacenesProducto = productoFind.almacenes;
+            const productoFind = await Productos.findById({ _id: productos[0]._id });
 
-            let nuevoMovimiento = {};
-            let almacenesList = almacenesProducto.map(almacen => {
-                let datos = {};
-                if(almacen.id == producto.almacen){
-                    datos.almacen = almacen.id;
-                    datos.despues = parseInt(nuevaCantidadAlmacen);
-                    datos.antes = parseInt(almacen.cantidad);
-                    datos.tipo = 'devuelto';
-                }else{
-                    datos.almacen = almacen.id;
-                    datos.despues = parseInt(almacen.cantidad);
-                    datos.antes = parseInt(almacen.cantidad);
-                    datos.tipo = 'devuelto';
+            const resultado = await Promise.all(
+                await productos.map(async producto => {
+
+                    const almacenDesc = productoFind.almacenes.filter(almacen => almacen.id === producto.almacen);
+                    const nuevaCantidadAlmacen = Number(almacenDesc[0].cantidad) + producto.cantidadVenta;
+                    const almacenesProducto = productoFind.almacenes;
+
+                    let nuevoMovimiento = {};
+                    let almacenesList = almacenesProducto.map(almacen => {
+                        let datos = {};
+                        if (almacen.id == producto.almacen) {
+                            datos.almacen = almacen.id;
+                            datos.despues = parseInt(nuevaCantidadAlmacen);
+                            datos.antes = parseInt(almacen.cantidad);
+                            datos.tipo = 'devuelto';
+                        } else {
+                            datos.almacen = almacen.id;
+                            datos.despues = parseInt(almacen.cantidad);
+                            datos.antes = parseInt(almacen.cantidad);
+                            datos.tipo = 'devuelto';
+                        }
+
+                        return datos;
+                    })
+
+
+                    nuevoMovimiento.almacenes = almacenesList;
+                    nuevoMovimiento.producto = productoFind.nombre;
+                    nuevoMovimiento.productoId = producto._id;
+                    nuevoMovimiento.usuario = usuario;
+                    const movimiento = new Movimientos(nuevoMovimiento);
+                    await movimiento.save();
+
+                    let nuevosAlmacenes = productoFind.almacenes.map(almacen => {
+                        if (almacen.id === producto.almacen) {
+                            almacen.cantidad = nuevaCantidadAlmacen;
+                            return almacen;
+                        }
+                        return null;
+                    });
+
+                    nuevosAlmacenes = nuevosAlmacenes.filter(item => item !== null);
+
+                    return nuevosAlmacenes[0]
+
+                })
+            )
+
+            const newStock = productoFind.almacenes.map(almacen => {
+
+                const findAlmacen = resultado.find(item => item.id === almacen.id);
+
+                if (findAlmacen) {
+                    return findAlmacen
                 }
-                    
-                return datos;
-            })
-            
 
-            nuevoMovimiento.almacenes = almacenesList;
-            nuevoMovimiento.producto = productoFind.nombre;
-            nuevoMovimiento.productoId = producto._id;
-            nuevoMovimiento.usuario = usuario;
-            const movimiento = new Movimientos(nuevoMovimiento);
-            await movimiento.save();
-
-            const nuevosAlmacenes = productoFind.almacenes.map( almacen => {
-                if(almacen.id === producto.almacen){
-                    almacen.cantidad = parseInt(nuevaCantidadAlmacen);
-                }
                 return almacen;
-            });
-
-            await Productos.findByIdAndUpdate({_id:producto._id},{
-                $set:{
-                    almacenes:nuevosAlmacenes
-                }
             })
+
+            await Promise.all(
+                await resultado.map(async item => {
+                    await saveNewStock(newStock, productos[0]._id);
+                })
+            )
 
         })
     )
 
 }
 
-exports.guardarDatosReporte = async (productos,vendedor,cliente,total,formaPago,divisa,estatus,noNota,venta) => {
+exports.guardarDatosReporte = async (productos, vendedor, cliente, total, formaPago, divisa, estatus, noNota, venta) => {
 
     await Promise.all(
-        await productos.map( async producto => {
+        await productos.map(async producto => {
 
-            const productoFind = await Productos.findById({_id:producto._id});
-            const clienteFind = await Clientes.findById({_id:cliente});
+            const productoFind = await Productos.findById({ _id: producto._id });
+            const clienteFind = await Clientes.findById({ _id: cliente });
 
             const sector = clienteFind.sector;
 
             let nuevoReporte = {};
-            
+
             nuevoReporte.remision = noNota;
             nuevoReporte.concepto = productoFind.nombre;
             nuevoReporte.cliente = cliente;
@@ -215,8 +282,8 @@ exports.guardarDatosReporte = async (productos,vendedor,cliente,total,formaPago,
             nuevoReporte.precio = total;
             nuevoReporte.descuento = producto.descuento;
             nuevoReporte.cantidad = producto.cantidadVenta;
-            nuevoReporte.precioProducto =  (producto.precioVenta - ((producto.descuento * producto.precioVenta) / 100));
-        
+            nuevoReporte.precioProducto = (producto.precioVenta - ((producto.descuento * producto.precioVenta) / 100));
+
             const reporte = new Reportes(nuevoReporte);
 
             reporte.save();
